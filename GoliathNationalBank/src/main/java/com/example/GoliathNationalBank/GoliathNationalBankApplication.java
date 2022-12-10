8f61360e8dabdcf96350f221a3fcd690dfd33a42
hector <hectorpelaez11@hotmail.com>
2022-12-10 13:30:54 -0500

    spring boot

---
+++ GoliathNationalBank/src/main/java/com/example/GoliathNationalBank/GoliathNationalBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.GoliathNationalBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoliathNationalBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoliathNationalBankApplication.class, args);
+	}
+
+}
---
+++ GoliathNationalBank/src/test/java/com/example/GoliathNationalBank/GoliathNationalBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GoliathNationalBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoliathNationalBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
